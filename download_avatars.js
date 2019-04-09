6a20a39323ed86441ff54ad3b5f58f3044599628
Julian <julian@juliantomlindesign.com>
2019-04-09 01:57:53 +0000

    added error prevention

--- download_avatars.js
+++ download_avatars.js
@@ -37,8 +37,23 @@ function downloadImageByURL(url, filePath) {
          })
          .pipe(fs.createWriteStream(filePath))
 }
-if (!repo){
+if (!repo) {
   console.log ('please include owener and repo name')
+}
+else if (process.argv[4]) {
+  console.log ('please only enter in the owner and repo names')
+}
+else if (!fs.existsSync('./avatar_pictures/')) {
+  console.log ('make sure ./avatar_pictures/ file exists in root directory')
+}
+else if (!fs.existsSync('./.env')) {
+  console.log ('error: missing ".env" file')
+}
+else if (!process.env.GITHUB_TOKEN) {
+  console.log ('GITHUB_TOKEN missing from ".env" file')
+}
+else if (process.env.GITHUB_TOKEN.length !== 40){
+  console.log ('please use a valid personal access token for GITHUB_TOKEN')
 }
 else {
   getRepoContributors(owner, repo, downloadImageByURL)
